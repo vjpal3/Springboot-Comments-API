92e9984b1815737fe106579c3f9d4e54caa5be68
vjpal3 <vrishalipal@yahoo.com>
2020-02-08 14:49:51 -0500

    Check a string for spam words

--- spamdetection/src/main/java/com/mycompany/commentsapi/spamdetection/SimpleSpamDetector.java
+++ spamdetection/src/main/java/com/mycompany/commentsapi/spamdetection/SimpleSpamDetector.java
@@ -1,5 +1,22 @@
 package com.mycompany.commentsapi.spamdetection;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimpleSpamDetector {
+	private List<String> spamWords = new ArrayList<String>();
+
+	public SimpleSpamDetector(List<String> spamWords) {
+		this.spamWords = spamWords;
+	}
+
+	public boolean containsSpam(String value) {
+		for (String spam : spamWords) {
+			if (value.toLowerCase().contains(spam)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
